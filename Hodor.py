dab1e1a331ba6ca75bbf301c06a0dd9dc11ebdab
Jarwin <jarwin005@gmail.com>
2016-02-02 02:47:25 +0530

    Bot will now automatically detect the last UID

--- Hodor.py
+++ Hodor.py
@@ -3,8 +3,21 @@
 import random
 
 def randomMessage(text):
-    text = text.split()
-    repeat = (random.randint(1,50)/random.randint(1,5))-1
+    text_len = len(text.split())
+    repeat = (random.randint(1,text_len)+random.randint(0,5))
+    punctuation = punctuations()
     hodor = "hodor "*repeat
-    msg = "Hodor "+hodor.strip()+"."
+    msg = "Hodor "+hodor.strip()+punctuation
     return msg
+
+def punctuations():
+    letter = random.randint(0,300)
+    repetation = random.randint(2,5)
+    punc = ""
+    if letter < 150:
+        punc = '!'*repetation
+    if letter > 150 and letter < 290:
+        punc = '.'*repetation
+    else:
+        punc = '?'*(repetation-1)
+    return punc
--- connect.py
+++ connect.py
@@ -4,17 +4,22 @@ import json
 import time
 
 import authkeys
+import Hodor
 
 class Updates():
     def __init__(self):
         self.token = authkeys.token()
-        self.offset = 0
+        self.offset = ""
         self.manager()
 
     def manager(self):
         updated_data = self.update()
         message_list = self.json_to_list(updated_data)
+        time.sleep(5)
+        self.recurser()
 
+    def recurser(self):
+        self.manager()
 
     def json_to_list(self, json_data):
         messages = len(json_data['result'])
@@ -24,15 +29,18 @@ class Updates():
             chat_id = msg['message']['chat']['id']
             try:
                 text = msg['message']['text']
+                print "{} : {}".format(self.offset, text)
                 message = Hodor.randomMessage(text)
             except KeyError:
-                message = "Hodor????"
-            self.send_message(chat_id,message)
-        self.offset = self.offset+1
+                message = "Hodor???????"
+            self.send(chat_id,message)
+        #hail marry
+        if json_data['result']:
+            self.offset = self.offset+1
 
 
     def send(self, chatid, message):
-        encode_dictonary = {'chat_id': str(chatid), 'text'=message}
+        encode_dictonary = {'chat_id': str(chatid), 'text':message}
         encoded = urllib.urlencode(encode_dictonary)
         send_message_url = r"https://api.telegram.org/bot"+self.token+r"/sendMessage?"+encoded
         m = urllib.urlopen(send_message_url)
@@ -40,9 +48,11 @@ class Updates():
 
 
     def update(self):
-        update_url  = r"https://api.telegram.org/bot"self.token+r"/getUpdates?offset="
-        update_url = update_url + str(self.offset)
+        update_url  = r"https://api.telegram.org/bot"+self.token+r"/getUpdates?offset="
+        call_url = update_url + str(self.offset)
         r = urllib.urlopen(call_url)
-        response = json.loads(r)
+        response = json.loads(r.read())
         return response
-        
+
+
+run = Updates()
