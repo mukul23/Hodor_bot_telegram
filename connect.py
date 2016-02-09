b9c65c9a45b0eb38442cc25c6dc04b548701225c
Purusharth Saxena <saxena.puru005@gmail.com>
2016-02-09 19:54:42 +0530

    Added comments
    Added try/catch clause for I/O Exception inside update method

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ import urllib
 import json
 import time
 
-import authkey
+import authkeys
 import Hodor
 
 class Updates():
@@ -13,29 +13,33 @@ class Updates():
         self.manager()
 
     def manager(self):
-        updated_data = self.update()
-        message_list = self.json_to_list(updated_data)
-        time.sleep(5)
-        self.recurser()
+        while True:
+            time.sleep(15) #Update interval
+            updated_data = self.update() #Checking for new messages
+            if not updated_data: #in case there's an IO error
+                continue
+            message_list = self.json_to_list(updated_data) #anatomy of the json response
+            continue
 
-    def recurser(self):
-        self.manager()
 
     def json_to_list(self, json_data):
-        messages = len(json_data['result'])
+        messages = len(json_data['result']) #nubmer of new messages since the last update
 
-        for msg in json_data['result']:
-            self.offset = msg['update_id']
-            chat_id = msg['message']['chat']['id']
+        for msg in json_data['result']:  #going through each new message one at a time.
+            self.offset = msg['update_id'] #keeping track of the update ID
+            chat_id = msg['message']['chat']['id'] #chat ID the user (for the current update_id)
             try:
+                '''Added try and chatch because
+                 the following Keys will not be avilable if user sends a image/sticker/video etc
+                 (Anything except text-messages)'''
                 text = msg['message']['text']
                 print "{} : {}".format(self.offset, text)
-                message = Hodor.randomMessage(text)
+                message = Hodor.randomMessage(text) #getting a random `HODOR` message
             except KeyError:
                 message = "Hodor???????"
-            self.send(chat_id,message)
+            self.send(chat_id,message) #calling the send function to send the message to the cuurent chat_id and update_id
         #hail marry
-        if json_data['result']:
+        if json_data['result']: #Updating the offset for the next update query
             self.offset = self.offset+1
 
 
@@ -50,9 +54,15 @@ class Updates():
     def update(self):
         update_url  = r"https://api.telegram.org/bot"+self.token+r"/getUpdates?offset="
         call_url = update_url + str(self.offset)
-        r = urllib.urlopen(call_url)
-        response = json.loads(r.read())
+        try:
+            r = urllib.urlopen(call_url)
+            response = json.loads(r.read())
+        except IOError:
+            time.sleep(2)
+            print "Bypassing [IOError]"
+            return []
         return response
 
 
 run = Updates()
+
